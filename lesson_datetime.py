976c6ad10b6bd0ad84d3d5fac66f484d1aa0f629
ilyakudryashov <36332497+ilyakudryashov@users.noreply.github.com>
2018-02-10 17:36:45 +0300

    -

--- lesson_datetime.py
+++ lesson_datetime.py
@@ -9,5 +9,5 @@ print(current_day)
 print(current_month)
 print(current_year)
 
-# Дата в дургом формате rgrtrtrt
+# Дата в дургом формате rgrtrtrth
 print('%s-%s-%s' % (now.month, now.day, now.year))
