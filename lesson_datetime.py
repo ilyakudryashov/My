d090d20e258da3d434eace2f5abaaefe74f35f64
ilyakudryashov <36332497+ilyakudryashov@users.noreply.github.com>
2018-02-10 17:29:08 +0300

    Initial commit

---
+++ lesson_datetime.py
@@ -0,0 +1,13 @@
+from datetime import datetime
+now = datetime.now()
+current_year = now.year
+current_month = now.month
+current_day = now.day
+
+print(now)
+print(current_day)
+print(current_month)
+print(current_year)
+
+# Дата в дургом формате
+print('%s-%s-%s' % (now.month, now.day, now.year))
